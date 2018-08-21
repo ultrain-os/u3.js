bb057e11b5873bf109c50e4254da77d9f0674b5e
刘飞飞 <liuff@liufeifeideMacBook-Pro.local>
2018-08-21 16:49:14 +0800

    add static method findTxByBlockId

--- src/model/block.js
+++ src/model/block.js
@@ -4,7 +4,20 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-let BlockSchema = new Schema();
+let BlockSchema = new Schema({
+    block_id : String,
+    block_num : Number,
+    block : Object
+});
+
+BlockSchema.static('findTxByBlockId', async function (block_num) {
+    const res = await this.findOne({block_num});
+    let trx = [];
+    if(res){
+        trx = res.block.transactions;
+    }
+    return trx;
+});
 
 BlockSchema.set('toJSON', {getters: true, virtuals: true});
 BlockSchema.set('toObject', {getters: true, virtuals: true});
