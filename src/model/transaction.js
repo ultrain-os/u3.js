d6f86bbb6b2c1759bd9e98fa683b9491d6150bd4
刘飞飞 <liuff@liufeifeideMacBook-Pro.local>
2018-08-21 21:04:56 +0800

    add static mehtod getActionsById

--- src/model/transaction.js
+++ src/model/transaction.js
@@ -4,11 +4,22 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-let TxSchema = new Schema()
+let TxSchema = new Schema({
+    trx_id : String,
+    actions : Object
+})
+
+TxSchema.static('getActionsById',async function(trx_id){
+    const res = await this.findOne({ trx_id });
+    let actions = [];
+    if(res){
+        actions = res.actions;
+    }
+    return actions;
+})
 
 TxSchema.set('toJSON', {getters: true, virtuals: true})
 TxSchema.set('toObject', {getters: true, virtuals: true})
-
 
 let Txs = mongoose.model("transactions", TxSchema)
 module.exports = Txs
