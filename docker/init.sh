3660b8ca45fa1eebbe36b189ac451be9fb3cde2b
benyasin <rgwapkk520>
2018-08-31 10:24:44 +0800

    log output to host

--- docker/init.sh
+++ docker/init.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 sleep 1
-rm -rf /root/.local/share/ultrainio/nodultrain/data && ./nodultrain
+rm -rf /root/.local/share/ultrainio/nodultrain/data && touch /data/log/log.txt && chmod 777 /data/log/log.txt && ./nodultrain > /data/log/log.txt 2>&1
