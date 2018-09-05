8ea57e9055180edcfb398e630e08bbe86441b46d
刘飞飞 <liuff@liufeifeideMacBook-Pro.local>
2018-09-05 16:02:43 +0800

    修改 history api 用例

--- test/history.test.js
+++ test/history.test.js
@@ -8,54 +8,57 @@ describe('history', async () => {
     it("getAllBlocks", async () => {
         const rs = await u3Instance.getAllBlocks(1,10,{},{_id:-1});
         console.log(rs);
-        assert.ok(1==1);
+        assert.ok(rs);
     });
 
     it("getContracts", async () => {
         const rs = await u3Instance.getContracts(1,10,{},{_id:-1});
         console.log(rs);
-        assert.ok(1==1);
+        assert.ok(rs);
     });
     
     it("getContractByName", async () => {
         const rs = await u3Instance.getContractByName("ultrainio");
         console.log(rs);
-        assert.ok(1==1);
+        assert.ok(rs);
     });
 
     it("getAllAccounts", async () => {
         const rs = await u3Instance.getAllAccounts(1,10,{},{_id:-1});
         console.log(rs);
-        assert.ok(1==1);
+        assert.ok(rs);
     });
 
     it("getAllTxs", async () => {
         const rs = await u3Instance.getAllTxs(1,10,{},{_id:-1});
         console.log(rs);
-        assert.ok(1==1);
+        assert.ok(rs);
     });
 
     it("getTxByTxId", async () => {
-        const rs = await u3Instance.getTxByTxId("0df6e9f46dc72243619cb6b4e8053ead6a0b612a3f1673622f242e9d1797f7d0");
+        const txs = await u3Instance.getAllTxs(1,1,{},{_id:-1});
+        const rs = await u3Instance.getTxByTxId(txs.results[0].trx_id);
         console.log(rs);
-        assert.ok(1==1);
+        assert.ok(rs);
     });
 
     it("getActionsByTxid", async () => {
-        const rs = await u3Instance.getActionsByTxid("0df6e9f46dc72243619cb6b4e8053ead6a0b612a3f1673622f242e9d1797f7d0");
+        const txs = await u3Instance.getAllTxs(1,1,{},{_id:-1});
+        const rs = await u3Instance.getActionsByTxid(txs.results[0].trx_id);
         console.log(rs);
-        assert.ok(1==1);
+        assert.ok(rs);
     });
 
     it("getActionsByAccount", async () => {
         const rs = await u3Instance.getActionsByAccount("ultrainio");
         console.log(rs);
-        assert.ok(1==1);
+        assert.ok(rs);
     });
 
     it("getTxsByBlockId", async () => {
-        const rs = await u3Instance.getTxsByBlockId("2");
+        const blocks = await u3Instance.getAllBlocks(1,1,{},{_id:-1});
+        const rs = await u3Instance.getTxsByBlockId(blocks.results[0].block_num);
         console.log(rs);
-        assert.ok(1==1);
+        assert.ok(rs);
     });
 })
