05f0a3f47c41500ffe211b0338e835a76f2600b6
benyasin <rgwapkk520>
2018-10-01 09:53:34 +0800

    always 8 users

--- docker-testnet/start.sh
+++ docker-testnet/start.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-rm -rf ../src/scratch
 docker rm -f inst2
 docker build -t registry.cn-hangzhou.aliyuncs.com/ultrain/ultraind:2.0.0 .
 docker run -itd -p 127.0.0.1:27018:27017 -p 127.0.0.1:8888:8888 -p 127.0.0.1:3000:3000 --name=inst2 -v $HOME/log:/data/log registry.cn-hangzhou.aliyuncs.com/ultrain/ultraind:2.0.0
