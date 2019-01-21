f2da8c01f3d969211f761525ba5f067c13baa532
ben <ben.yasin80@gmail.com>
2019-01-21 18:26:30 +0800

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
-var devNet = {
-  httpEndpoint: 'http://172.16.10.3:8899',
-  httpEndpoint_history: 'http://172.16.10.3:8899',
+var config = {
+  httpEndpoint: 'http://127.0.0.1:8888',
+  httpEndpoint_history: 'http://127.0.0.1:3000',
   broadcast: true,
   debug: false,
   verbose: false,
@@ -15,21 +15,4 @@ var devNet = {
   symbol: 'UGAS'
 };
 
-var testNet = {
-  httpEndpoint: 'http://benyasin.s1.natapp.cc',
-  httpEndpoint_history: 'http://history.natapp1.cc',
-  broadcast: true,
-  debug: false,
-  verbose: false,
-  sign: true,
-  logger: {
-    log: console.log,
-    error: console.error,
-    debug: console.log
-  },
-  chainId:'262ba309c51d91e8c13a7b4bb1b8d25906135317b09805f61fcdf4e044cd71e8',
-  binaryen: require('binaryen'),
-  symbol: 'UGAS'
-};
-
-module.exports = devNet;
+module.exports = config;
