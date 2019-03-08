36855127223368f91c215696a8e1d45395054db4
benyasin <rgwapkk520>
2019-03-08 11:41:02 +0800

    rename httpEndpoint_history to httpEndpointHistory

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   httpEndpoint: 'http://127.0.0.1:8888',
-  httpEndpoint_history: 'http://127.0.0.1:3000',
+  httpEndpointHistory: 'http://127.0.0.1:3000',
   broadcast: true,
   debug: false,
   verbose: false,
@@ -11,6 +11,5 @@ module.exports = {
     debug: console.log
   },
   chainId:'baf8bb9d3636379e3cd6779d2a72e693494670f1040d45154bb61dc8852c8971',
-  binaryen: require('binaryen'),
   symbol: 'UGAS'
 };
